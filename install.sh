6ca6ac2832bb7ddbf99c56db75c798c0da692d21
Ayokunle Sunmola <se.ayokunle@gmail.com>
2024-01-29 18:38:57 -0800

    hh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pip3 install coloredlogs verboselogs python-dotenv Flask  torch torchvision torchaudio transformers alpaca-py lumibot timedelta
+pip3 install coloredlogs verboselogs python-dotenv Flask  torch torchvision torchaudio transformers alpaca-py lumibot timedelta unittest
